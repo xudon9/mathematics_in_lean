592e1fb2d46a1399071c13b6e3f6259619b48f3f
Xudong Wang <xudon9@outlook.com>
2025-03-15 20:03:36 +0800

    Finish C02/S04

--- MIL/C02_Basics/S04_More_on_Order_and_Divisibility.lean
+++ MIL/C02_Basics/S04_More_on_Order_and_Divisibility.lean
@@ -82,11 +82,17 @@ theorem aux : min a b + c ≤ min (a + c) (b + c) := by
 example : min a b + c = min (a + c) (b + c) := by
   apply le_antisymm
   · apply aux
-  sorry -- TODO
+  · nth_rw 2 [← add_neg_cancel_right a c, ← add_neg_cancel_right b c]
+    apply add_neg_le_iff_le_add.mp
+    apply aux
+
 #check (abs_add : ∀ a b : ℝ, |a + b| ≤ |a| + |b|)
 
-example : |a| - |b| ≤ |a - b| :=
-  sorry
+example : |a| - |b| ≤ |a - b| := by
+  rw [sub_eq_add_neg]
+  apply add_neg_le_iff_le_add.mpr
+  nth_rw 1 [←  sub_add_cancel a b]
+  apply abs_add
 end
 
 section
@@ -103,7 +109,13 @@ example : x ∣ x ^ 2 := by
   apply dvd_mul_left
 
 example (h : x ∣ w) : x ∣ y * (x * z) + x ^ 2 + w ^ 2 := by
-  sorry
+  apply dvd_add
+  apply dvd_add
+  · apply dvd_mul_of_dvd_right
+    apply dvd_mul_right
+  · apply dvd_mul_left
+  · apply dvd_mul_of_dvd_right
+    assumption
 end
 
 section
@@ -115,5 +127,11 @@ variable (m n : ℕ)
 #check (Nat.lcm_zero_left n : Nat.lcm 0 n = 0)
 
 example : Nat.gcd m n = Nat.gcd n m := by
-  sorry
+  have h (a b : ℕ): Nat.gcd a b ∣ Nat.gcd b a := by
+    apply dvd_gcd
+    exact gcd_dvd_right a b
+    exact gcd_dvd_left a b
+  apply dvd_antisymm
+  apply h
+  apply h
 end
