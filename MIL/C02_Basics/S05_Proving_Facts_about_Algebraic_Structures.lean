36190762d486a61a93be37be116002eed48e2d7f
Xudong Wang <xudon9@outlook.com>
2025-03-22 22:15:15 +0800

    Finish C02 S05

--- MIL/C02_Basics/S05_Proving_Facts_about_Algebraic_Structures.lean
+++ MIL/C02_Basics/S05_Proving_Facts_about_Algebraic_Structures.lean
@@ -48,10 +48,18 @@ example : x ⊔ y ⊔ z = x ⊔ (y ⊔ z) := by
   sorry
 
 theorem absorb1 : x ⊓ (x ⊔ y) = x := by
-  sorry
+  apply le_antisymm
+  · apply inf_le_left
+  · apply le_inf
+    apply le_refl
+    apply le_sup_left
 
 theorem absorb2 : x ⊔ x ⊓ y = x := by
-  sorry
+  apply le_antisymm
+  · apply sup_le
+    apply le_refl
+    apply inf_le_left
+  · apply le_sup_left
 
 end
 
@@ -69,11 +77,35 @@ section
 variable {α : Type*} [Lattice α]
 variable (a b c : α)
 
-example (h : ∀ x y z : α, x ⊓ (y ⊔ z) = x ⊓ y ⊔ x ⊓ z) : a ⊔ b ⊓ c = (a ⊔ b) ⊓ (a ⊔ c) := by
-  sorry
+example (h : ∀ x y z : α, x ⊓ (y ⊔ z) = x ⊓ y ⊔ x ⊓ z) : a ⊔ b ⊓ c = (a ⊔ b) ⊓ (a ⊔ c) :=
+  calc
+    a ⊔ b ⊓ c = (a ⊔ a ⊓ c) ⊔ (b ⊓ c) := by
+      nth_rw 1 [← absorb2 a c]
+    _ = a ⊔ ((a ⊓ c) ⊔ (b ⊓ c)) := by
+      rw [sup_assoc]
+    _ = a ⊔ ((a ⊔ b) ⊓ c) := by
+      rw [inf_comm a c, inf_comm b c, inf_comm (a ⊔ b) c, ← h ]
+    _ = ((a ⊔ b) ⊓ a) ⊔ ((a ⊔ b) ⊓ c) := by
+      rw [inf_comm (a ⊔ b) a,  absorb1]
+    _ = (a ⊔ b) ⊓ (a ⊔ c) := by
+      rw [← h]
 
-example (h : ∀ x y z : α, x ⊔ y ⊓ z = (x ⊔ y) ⊓ (x ⊔ z)) : a ⊓ (b ⊔ c) = a ⊓ b ⊔ a ⊓ c := by
-  sorry
+example (h : ∀ x y z : α, x ⊔ y ⊓ z = (x ⊔ y) ⊓ (x ⊔ z)) : a ⊓ (b ⊔ c) = a ⊓ b ⊔ a ⊓ c :=
+  calc
+    a ⊓ (b ⊔ c) = a ⊓ (c ⊔ b) := by
+      rw [sup_comm b c]
+    _ = a ⊓ (c ⊔ a)  ⊓  (c ⊔ b) := by
+      nth_rw 1 [← absorb1 a c, sup_comm a c]
+    _ = a ⊓ ((c ⊔ a) ⊓ (c ⊔ b)) := by
+      rw [inf_assoc]
+    _ = a ⊓ (c ⊔ a ⊓ b) := by
+      rw [← h]
+    _ = a ⊓ (a ⊓ b ⊔ c) := by
+      rw [sup_comm c (a ⊓ b)]
+    _ = (a ⊓ b ⊔ a) ⊓ (a ⊓ b ⊔ c) := by
+      nth_rw 1 [← absorb2 a b, sup_comm a]
+    _ = a ⊓ b ⊔ a ⊓ c := by
+      rw [← h]
 
 end
 
@@ -87,13 +119,27 @@ variable (a b c : R)
 #check (mul_nonneg : 0 ≤ a → 0 ≤ b → 0 ≤ a * b)
 
 example (h : a ≤ b) : 0 ≤ b - a := by
-  sorry
+  rw [← neg_add_cancel a, sub_eq_add_neg, add_comm b]
+  apply add_le_add_left
+  apply h
 
 example (h: 0 ≤ b - a) : a ≤ b := by
-  sorry
+  rw [← zero_add a, add_comm, ← add_neg_cancel_right b a,
+    add_comm b a, add_assoc, ← sub_eq_add_neg]
+  apply add_le_add_left
+  apply h
 
 example (h : a ≤ b) (h' : 0 ≤ c) : a * c ≤ b * c := by
-  sorry
+  /- Essentially prove a*c + 0 ≤ a*c + (b-a)*c -/
+  rw [← zero_add (a * c), add_comm, ← add_neg_cancel_right b a,
+    add_comm b a, add_assoc, ← sub_eq_add_neg, add_mul]
+  apply add_le_add_left
+  apply mul_nonneg
+  · /- To prove 0 ≤ b - a, the same as Example above -/
+    rw [← neg_add_cancel a, sub_eq_add_neg, add_comm b]
+    apply add_le_add_left
+    apply h
+  · apply h'
 
 end
 
@@ -106,7 +152,12 @@ variable (x y z : X)
 #check (dist_triangle x y z : dist x z ≤ dist x y + dist y z)
 
 example (x y : X) : 0 ≤ dist x y := by
-  sorry
+  have h : 0 ≤ dist x y + dist x y := by
+    nth_rw 2 [dist_comm]
+    apply le_trans (b := dist x x)
+    · rw [dist_self]
+    · apply dist_triangle
+  linarith
 
 end
 
